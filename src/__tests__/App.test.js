fa2f04d7fbbfcf52469d8ef195727f533a843eeb
jisuyoun <nigayoun@gmail.com>
2020-06-21 16:55:58 +0900

    fix. src path

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {cleanup, fireEvent, render, waitForElement} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
-import App from './src/App.js'
+import App from '../App.js'
 
 afterEach(cleanup);
 
