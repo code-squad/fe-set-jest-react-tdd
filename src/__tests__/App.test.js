86a798edd51a89753d0d08c78d308b7c83bfa3cf
crong <crongro@gmail.com>
2020-11-16 16:56:31 +0900

    change. waitForElement deprecated 대응

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {cleanup, fireEvent, render, waitForElement} from '@testing-library/react';
+import {cleanup, fireEvent, render, waitFor} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import App from '../App.js'
 
 afterEach(cleanup);
 
 it('App.js: hello world가 상단에 노출된다', () => {
+    console.log(findBy);
     expect(true).toBeTruthy();
     const {queryByText} = render( <App />);
     expect(queryByText('hello world')).toBeInTheDocument();
